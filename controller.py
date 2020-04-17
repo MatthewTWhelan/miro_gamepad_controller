107b4e5c4e00f66d7760ddf0e66055610bc46633
Matt Whelan <matt_double_u@hotmail.co.uk>
2020-04-17 15:07:43 +0100

    Fixed some bugs and added a bash file to run the script without pygame's debug messages appearing.

--- controller.py
+++ controller.py
@@ -4,7 +4,6 @@ from std_msgs.msg import UInt8, UInt16, UInt32, Float32MultiArray, UInt16MultiAr
 from geometry_msgs.msg import Pose2D, TwistStamped
 from sensor_msgs.msg import JointState
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 import time
 import miro2 as miro
@@ -31,6 +30,12 @@ class MiroController:
 		self.msg_kin = JointState()
 		self.msg_kin.position = [0.0, np.radians(30.0), 0.0, 0.0]
 
+		# subscribe to kinematic joints
+		topic = topic_base_name + "/sensors/kinematic_joints"
+		self.sub_kin = rospy.Subscriber(topic, JointState, self.callback_kinematic_joints, queue_size=5,
+		                                tcp_nodelay=True)
+		self.kin_pos = [0.0, np.radians(30.0), 0.0, 0.0] #[tilt, lift, yaw, pitch]
+
 		# publish to cosmetic joints
 		topic = topic_base_name + "/control/cosmetic_joints"
 		self.pub_cos = rospy.Publisher(topic, Float32MultiArray, queue_size=0)
@@ -44,9 +49,15 @@ class MiroController:
 		self.joystick1 = pygame.joystick.Joystick(0)
 		self.joystick1.init()
 
+	def callback_kinematic_joints(self, msg):
+		self.kin_pos[0] = msg.position[0]
+		self.kin_pos[1] = msg.position[1]
+		self.kin_pos[2] = msg.position[2]
+		self.kin_pos[3] = msg.position[3]
 
 	def controller(self):
 		t_init = time.time()
+		self.reset_miro()
 		vel = 0
 		omega = 0
 		while not rospy.core.is_shutdown():
@@ -74,23 +85,23 @@ class MiroController:
 		# values range from -1 to 1 for the axis
 		LS_horizontal = joystick.get_axis(0)
 		LS_vertical = joystick.get_axis(1)
-		LT = joystick.get_axis(2)
-		RT = joystick.get_axis(5)
 		RS_horizontal = joystick.get_axis(3)
 		RS_vertical = joystick.get_axis(4)
-
-		# values take on either 1 if pressed or 0 otherwise
-		button_A = joystick.get_button(0)
-		button_B = joystick.get_button(1)
-		button_X = joystick.get_button(2)
-		button_Y = joystick.get_button(3)
-		button_LB = joystick.get_button(4)
-		button_RB = joystick.get_button(5)
-		button_BACK = joystick.get_button(6)
-		button_START = joystick.get_button(7)
-		button_MENU = joystick.get_button(8)
-		button_LS = joystick.get_button(9)
-		button_RS = joystick.get_button(10)
+		# LT = joystick.get_axis(2)
+		# RT = joystick.get_axis(5)
+		#
+		# # values take on either 1 if pressed or 0 otherwise
+		# button_A = joystick.get_button(0)
+		# button_B = joystick.get_button(1)
+		# button_X = joystick.get_button(2)
+		# button_Y = joystick.get_button(3)
+		# button_LB = joystick.get_button(4)
+		# button_RB = joystick.get_button(5)
+		# button_BACK = joystick.get_button(6)
+		# button_START = joystick.get_button(7)
+		# button_MENU = joystick.get_button(8)
+		# button_LS = joystick.get_button(9)
+		# button_RS = joystick.get_button(10)
 
 		# for directional buttons, hat is a tuple of (left_right, up_down) with values of 1 or -1 if right/up or
 		# left/down is pressed, respectively. Both horizontal and vertical can be simultaneously pressed,
@@ -98,83 +109,26 @@ class MiroController:
 		# hat = joystick.get_hat(0)
 		#
 		#
-		vel = - left_stick_vertical * 0.4
-		omega = - right_stick_horizontal * 5
+		vel = - LS_vertical * 0.4
+		omega = - RS_horizontal * 5
+
+		# To avoid small amounts of drift
 		if abs(vel) < 0.05:
 			vel = 0
 		if abs(omega) < 0.05:
 			omega = 0
 
-		if button_A:
-			self.victory_dance()
-		elif button_B:
-			self.head_shake_in_disappointment()
-
-
-		for i in range(joystick_count):
-			joystick = pygame.joystick.Joystick(i)
-			joystick.init()
-
-
-
-			# print("Joystick {}".format(i))
-
-			# Get the name from the OS for the controller/joystick
-			# name = joystick.get_name()
-			# print("Joystick name: {}".format(name))
-
-			# Usually axis run in pairs, up/down for one, and left/right for
-			# the other.
-			axes = joystick.get_numaxes()
-			# print("Number of axes: {}".format(axes))
-
-			# for i in range(axes):
-			# 	axis = joystick.get_axis(0)
-			#
-			# 	print("Axis {} value: {:>6.3f}".format(i, axis))
-			#
-			#
-			# buttons = joystick.get_numbuttons()
-			#
-			# print("Number of buttons: {}".format(buttons))
-			#
-			#
-			# for i in range(buttons):
-			# 	button = joystick.get_button(i)
-			#
-			# 	print("Button {:>2} value: {}".format(i, button))
-			#
-			#
-			# # Hat switch. All or nothing for direction, not like joysticks.
-			# # Value comes back in an array.
-			# hats = joystick.get_numhats()
-			#
-			# print("Number of hats: {}".format(hats))
-			#
-			#
-			# for i in range(hats):
-			# 	hat = joystick.get_hat(i)
-			#
-			# 	print("Hat {} value: {}".format(i, str(hat)))
-
+		# Mapping the buttons to sequences of behaviours
+		# if button_A == 1:
+		# 	self.victory_dance()
+		# 	button_A = 0
+		# elif button_B == 1:
+		# 	self.head_shake_in_disappointment()
+		# elif button_X == 1:
+		# 	self.reset_miro()
 
 		return vel, omega
 
-
-	def avoid_wall(self):
-		t_init = time.time()
-		self.msg_wheels.twist.linear.x = -0.1
-		self.msg_wheels.twist.angular.z = 0
-		while time.time() - t_init < 1:
-			self.pub_wheels.publish(self.msg_wheels)
-		p_anti_clock = 0.8
-		self.msg_wheels.twist.linear.x = 0
-		if np.random.rand() < p_anti_clock:
-			self.msg_wheels.twist.angular.z = 1.5
-		else:
-			self.msg_wheels.twist.angular.z = -1.5
-		while time.time() - t_init < 2:
-			self.pub_wheels.publish(self.msg_wheels)
 
 	def victory_dance(self):
 		t_init = time.time()
@@ -183,7 +137,8 @@ class MiroController:
 			self.msg_kin.position[1] = np.radians(t*15 + 30)
 			self.pub_kin.publish(self.msg_kin)
 			t = time.time() - t_init
-		time.sleep(15)
+		time.sleep(10)
+		self.reset_miro()
 
 	def head_shake_in_disappointment(self):
 		t_init = time.time()
@@ -206,7 +161,23 @@ class MiroController:
 		self.reset_miro()
 
 	def reset_miro(self):
-		return
+		# [tilt, lift, yaw, pitch] range of vals (in degs) are [unknown, 8 (up) to 60 (down), -50 (right) to 50 (
+		# left), -22 (up) to 8 (down)]
+
+		reset_position = [0.0, np.radians(30), np.radians(0), 0.0]
+		current_position = self.kin_pos
+		[diff_tilt, diff_lift, diff_yaw, diff_pitch] = [a - b for a, b in zip(current_position, reset_position)]
+		while abs(diff_lift) > 0.1 or abs(diff_pitch) > 0.05 or abs(diff_yaw) > 0.05:
+		# while abs(diff_lift) > 0.1:
+		# 	print("Updating")
+			print([diff_tilt, diff_lift, diff_yaw, diff_pitch])
+			current_position = self.kin_pos
+			[diff_tilt, diff_lift, diff_yaw, diff_pitch] = [a - b for a, b in zip(current_position, reset_position)]
+			self.msg_kin.position[0] += - 0.0005 * diff_tilt
+			self.msg_kin.position[1] += - 0.001 * diff_lift
+			self.msg_kin.position[2] += - 0.001 * diff_yaw
+			self.msg_kin.position[3] += - 0.001 * diff_pitch
+			self.pub_kin.publish(self.msg_kin)
 
 
 if __name__ == "__main__":
